6e05e7340099a269f3dc42eb8b5794cc1eb518f0
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-07-21 18:34:45 +0100

    update execute

---
+++ execute.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * execute_call - To execute commads from users
+ * @argv: argument variables
+ * @name: name of the shell
+ * Return: status
+ */
+int execute_call(char **argv, char *name)
+{
+	int status = 0, result;
+	char *full_path = NULL;
+
+	if (!argv || !*argv)
+		return (-1);
+
+	if (check_builtin(argv))
+		return (status);
+
+	if (access(argv[0], X_OK) == 0)
+	{
+		return (fork_command(argv[0], argv));
+	}
+	else
+	{
+		full_path = search_path(argv[0]);
+
+		if (full_path != NULL)
+		{
+			result = fork_command(full_path, argv);
+			return (result);
+		}
+	}
+
+	perror(name);
+	return (-1);
+}
