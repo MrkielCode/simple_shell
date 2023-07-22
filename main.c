9adc2b0c2b5f7579ec163a402c26fedb9cabedbb
olarich11 <olarich11@gmail.com>
2023-07-22 08:08:09 -0700

    updated main

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include "shell.h"
 /**
- * main - entry point
- * @argc: argument constant
- * @argv: argument variable
- * Return: 0;
+ * main - entry point for the shell program
+ * @argc: number of command-line argument constant
+ * @argv: array of stirngs containing the command-line
+ * argument variable
+ * Return: Always 0;
  */
 
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
+
 	{
-			my_interactive_shell(argv[0]);
+		my_interactive_shell(argv[0]);
 	}
+
 	return (0);
 }
