f7a66749390ab837338c9e92ff4b88277421e739
olarich11 <olarich11@gmail.com>
2023-07-26 10:36:25 -0700

    shell.h

--- execute.c
+++ execute.c
@@ -14,18 +14,15 @@ int execute_call(char **argv, char *name)
 	if (!argv || !*argv)
 		return (-1);
 
-	/ Check if the command is a built - in shell command
 	if (check_builtin(argv))
 		return (status);
 
-	/ Check if the command is accessible in the current directory
 	if (access(argv[0], X_OK) == 0)
 	{
 		return (fork_command(argv[0], argv));
 	}
 	else
 	{
-		/ Search for the command in the PATH envirnment variable
 		full_path = search_path(argv[0]);
 
 		if (full_path != NULL)
@@ -35,7 +32,6 @@ int execute_call(char **argv, char *name)
 		}
 	}
 
-	If the command is not found or cannot be executed, display an error
 	perror(name);
 	return (-1);
 }
