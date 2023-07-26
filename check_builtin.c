5f6ce067e50d413551fbc19ea765202343d1880f
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-07-26 15:08:02 +0100

    check builtin

--- check_builtin.c
+++ check_builtin.c
@@ -10,12 +10,10 @@ int check_builtin(char **av)
 {
 	if (!av[0])
 		return (0);
-	if (!strcmp(av[0], "exit"))
+	else if (!strcmp(av[0], "exit"))
 		s_exit(av);
-	if (!strcmp(av[0], "env"))
+	else if (!strcmp(av[0], "env"))
 		s_env();
-	if (!strcmp(av[0], "cd"))
-		s_cd(av);
 	else
 		return (0);
 
