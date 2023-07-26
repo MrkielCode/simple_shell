fef79d2eaa55b777dc1d1fcd0f10c1129ccaa7e0
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-07-26 15:50:23 +0100

    exit updated

--- shell_exit_func.c
+++ shell_exit_func.c
@@ -6,13 +6,42 @@
  * Return: void
  */
 
+/*
 void s_exit(char **av)
 {
 	int result = 0;
 
 	if (av[1] != NULL)
+	{
 		result = atoi(av[1]);
+	}
 
 	free_toks(av);
-	exit(result);
+}
+*/
+
+void s_exit(char **av)
+{
+	int j, k;
+
+	if (av[1])
+	{
+		k = atoi(av[1]);
+
+		if (k <= -1)
+		{
+			k = 2;
+		}
+
+		free_toks(av);
+		exit(k);
+	}
+
+	for (j = 0; av[j]; j++)
+	{
+		free(av[j]);
+	}
+
+	free(av);
+	exit(0);
 }
