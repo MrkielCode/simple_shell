fbe1c97860598e5c41b98900e8876cac4a0c4684
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-07-27 12:05:14 +0100

    fork_command.c

--- fork_command.c
+++ fork_command.c
@@ -1,47 +1,45 @@
 #include "shell.h"
+
 /**
  * fork_command - To fork command if true
+ *
  * @full_path: to get command or path
  * @argv: arrays of arguments
  *
- * Return: status
+ * Return: status;
  */
 
 int fork_command(char *full_path, char **argv)
 {
 	pid_t pid;
+	int status;
 
 	pid = fork();
 
 	if (pid < 0)
 	{
-		perror("fork error");
+		_print("fork error");
 		return (-1);
 	}
 
+	if (pid == -1)
+	{
+		perror(argv[0]);
+		return (-1);
+	}
 	if (pid == 0)
 	{
-		if (execve(full_path, argv, NULL) == -1)
+		if (execve(full_path, argv, environ) == -1)
 		{
-			perror("execve error");
-			exit(EXIT_FAILURE);
+			perror(argv[0]);
+			return (-1);
 		}
 	}
 	else
 	{
-		int status;
-
-		waitpid(pid, &status, 0);
-
-		if (WIFEXITED(status))
-		{
-			return (WEXITSTATUS(status));
-		}
-		else
-		{
-			perror("Child process did not terminate normally.");
-			return (-1);
-		}
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 
 	return (-1);
